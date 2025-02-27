a25414d492147fc8d348168b649de508a81afbd3
Ashahab007 <ashahab007@gmail.com>
2025-02-28 03:13:57 +0600

    1st conceptual problem soled

--- js/scriptByMe.js
+++ js/scriptByMe.js
@@ -1,5 +1,25 @@
 const ringButton = document.querySelectorAll(".ring-button");
-const ringButtonId = document.getElementById("purple-color");
-console.log(ringButtonId.classList);
+/* const ringButtonId = document.getElementById("purple-color");
+console.log(ringButtonId.classList); */
 
+for (let i = 0; i < ringButton.length; i++) {
+  const ringBtn = ringButton[i];
+  const productImageId = document.getElementById("product-image");
+  console.log(ringBtn);
+  //   purple color add করার আগে এই loop এ check করে নেওয়া হছে যদি purple color থাকে তাহলে সেটা remove করে দিবে। এবং নতুন color set করে দিবে ।
+  ringBtn.addEventListener("click", function (event) {
+    let color = event.target.id.replace("-color", ""); //replacing the '-color' name from purple-color for dynamically use in productImageId.src.
+    console.log(color);
+
+    for (let j = 0; j < ringButton.length; j++) {
+      ringButton[j].classList.remove("border-purple-600");
+      ringButton[j].classList.add("border-gray-300");
+    }
+    // adding purple color and remove gray color
+    event.target.classList.add("border-purple-600");
+    event.target.classList.remove("border-gray-300");
+
+    productImageId.src = "../images/" + color + ".png";
+  });
+}
 // alert();
